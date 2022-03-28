8a40f4776bd8517ccafa22169bca807288a37c52
Artur César <arturcesarsilva@gmail.com>
2022-03-27 22:07:52 -0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <Comments />
+</template>
+
+<script>
+import Comments from "./components/Comment-app.vue"
+export default {
+  name: "app",
+  components: {
+    Comments,
+  },
+};
+</script>
+
+<style>
+</style>
